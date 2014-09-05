35facee76e07f3254b9fdd740dfcedd1c0e24c48
sree1127 <sreedevi.vedula@RACKSPACE.COM>
2014-09-05 22:12:34 +0530

    Removed features as they are available in another branch.

--- features/step_definitions/login_steps.js
+++
@@ -1,6 +0,0 @@
-var myStepDefinitionsWrapper = function () {
-    this.Given(/^I am on Login Page$/, function (callback) {
-        callback.pending();
-    });
-};
-module.exports = myStepDefinitionsWrapper;
